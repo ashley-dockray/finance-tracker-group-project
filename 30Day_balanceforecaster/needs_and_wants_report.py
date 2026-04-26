4b83a38552097ff3bb5f39d2f0b451155f22b355
Frederick Bullen <freddybullen@icloud.com>
2026-04-26 16:25:35 +0100

    Refactor balance forecasting and add report feature
    Refactor bill filtering and balance calculation logic. Add needs vs wants report functionality.

--- 30Day_balanceforecaster/needs_and_wants_report.py
+++ 30Day_balanceforecaster/needs_and_wants_report.py
@@ -1,17 +1,17 @@
 import json
 from datetime import datetime, timedelta
-from transaction_manager import view_transactions
+from transaction_manager import transactions
 from core_classes import Income, Expense, RecurringBill
 
 def Bill_filter(): # this filteres the dat to make sure we get only the bills from the saved data
-    jason_file_data = view_transactions()
+    jason_file_data = transactions
     filtered_data = []
 
     current_date = datetime.now().date() # we use the date as one form of filtering the data, ensuring all the bills are in date
     due_date = current_date + timedelta(days=30)
 
     for data in jason_file_data: 
-        if type(data).__name__ == "RecurringBill": #checks if the object is a recurring bill
+        if isinstance(data, RecurringBill): #checks if the object is a recurring bill
             next_due = data.get_next_due()
             if isinstance(next_due, datetime): # this gets the next due date and converts it into an actual date format
                 next_due = next_due.date()
@@ -21,7 +21,7 @@ def Bill_filter(): # this filteres the dat to make sure we get only the bills fr
     return filtered_data
 
 def balance_calculator(): # knowing the current ballance is important so this function works it out based on expenses and income
-    jason_file_data = view_transactions()
+    jason_file_data = transactions
     balance= 0 
     for data in jason_file_data: # this for loop checks if the data is income or expense and decides weather or not to add or subtract the money from the ballance
         if isinstance(data, Income):
@@ -37,7 +37,7 @@ def forcast_over_next_30_days(): #  this runs through the next 30 days and keeps
     current_date = datetime.now().date()
     forecast = []
 
-    for day in range(31): #interates through each day
+    for day in range(30): #iterates through each day
         date = current_date + timedelta(days=day)
         
         for bill in bills: #this checks the dates and works out weather or not the cost should be subtracted from the balance
@@ -52,10 +52,43 @@ def forcast_over_next_30_days(): #  this runs through the next 30 days and keeps
     return forecast
 
 def alarm_system(forecast_list, safety_limit): # this is an alarm sytem function which compares each peice of data in the forcast list to a pre determined limmit
-    for date, balance in forecast_list: #iterates through the list
-        if balance < safety_limit: 
-            return True #returns true is the they are below the limit
+    for date, balance in forecast_list: # iterates through a list
+        if balance < safety_limit:
+            return True # returns true if they are below the limit
     return False 
+
+def needs_vs_wants_report(): # this function returns a report on the relationship between the needed and wanted expenses
+    jason_file_data = transactions
+    needs = []
+    needs_count = 0
+    wants = []
+    wants_count = 0
+    for data  in jason_file_data: 
+        if isinstance(data, Expense): # it itterates through each bit of data and ignores anything which isnt an expense 
+            importance = data.get_importance()
+            if importance == "need": # it seperates the needs and the wants keeping a running tally of the overall spendage
+                needs.append(data)
+                needs_count  += data.get_amount()
+            elif importance == "want":
+                wants.append(data)  
+                wants_count += data.get_amount()
+
+    total = needs_count + wants_count
+    if total > 0 :   #  it also works out the precentage of of the needs spendage against the wants
+        needs_percent = (needs_count / total) * 100
+        wants_percent = (wants_count / total) * 100
+    else:
+        needs_percent = wants_percent = 0
+    return {"needs_list": needs, "wants_list": wants, "needs_total": needs_count, "wants_total": wants_count, "needs_percentage": needs_percent, "wants_percentage": wants_percent}
+        
+
+
+
+
+
+    
+
+
 
 
 
